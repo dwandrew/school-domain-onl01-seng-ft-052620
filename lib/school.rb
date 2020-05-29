0561e3ece4b244d242d7e4d59764616efd272e7f
Daniel Andrew <danwandrew@gmail.com>
2020-05-29 08:31:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,14 @@
 # code here!
 class School
-  attr_reader :name
+  attr_reader :name :roster
   
 def initialize(name)
   @name =name
+  roster =[]
+end
+
+def add_student(name, grade)
+  roster<< {grade: [name]}
 end
 
 end
